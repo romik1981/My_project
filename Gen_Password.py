5b8212f91b485c460cd674092d033e34c3d7ed98
Belyakov Roman <belyakov1981@mail.ru>
2022-06-18 22:23:19 +0300

    intermediate version

--- Gen_Password.py
+++ Gen_Password.py
@@ -2,6 +2,7 @@
 Программа для генерации паролей по определённым правилам!
 '''
 import random
+import sys
 
 cap_let = [chr(let) for let in range(ord('A'), ord('Z') +1)]
 low_let = [chr(let) for let in range(ord('a'), ord('z') + 1)]
@@ -15,8 +16,8 @@ low_let.remove('l'), cap_let.remove('I'), cap_let.remove('O')
 #password_str = ''.join(password_list)
 #a = round(random.random() * 10)
 #print(random.sample(cap_let, 5))
-def get_password(n_cap, n_low, n_num, n_sym) -> str:
-    cap_let_list =list(random.sample(cap_let, n_cap))
+def get_password(n_cap, n_low, n_num, n_sym, can_repeat=False) -> str:
+    cap_let_list = list(random.sample(cap_let, n_cap))
     low_let_list = list(random.sample(low_let, n_low))
     num_list = list(random.sample(num, n_num))
     symbol_list = list(random.sample(symbol, n_sym))
@@ -25,16 +26,292 @@ def get_password(n_cap, n_low, n_num, n_sym) -> str:
     password_str = ''.join(password_list)
     return password_str
 
+   # for _ in range(k_sym_pass):
+
+
 print('Программа для генерации паролей длиной не более 30 символов!',
-      'Состоящие, по умолчанию, из заглавных и строчных букв латинского алфавита,','цифр и некоторых символов.',
+      'Состоящие, по умолчанию, из заглавных и строчных букв латинского алфавита, цифр и некоторых символов.',
       'Примечание: В паролях отсутствуют буквs "O", "l", "I", так как они похожи на некоторые цифры и друг на друга.\n',
       sep='\n')
-#k_sym_pass = int(input('Введите число символов пароля: '))
-k_sym_pass = 8
-n_cap = round(k_sym_pass / 4)
-n_low = round(k_sym_pass / 4)
-n_num = round(k_sym_pass  * 7 / 20)
-n_sym = round(k_sym_pass * 3 /  20)
-print('Ваш пароль:',get_password(n_cap, n_low, n_num, n_sym))
+
+k_sym_pass, quantity_pass = input('Введите число символов пароля: '), input('Введите число паролей: ')
+symbol_password = input('Введите символы используемые в паролях (c - заглавные, l - строчные, n - цифры, s - символы): ')
+save_file = input('Сохранить данные в файл? y/n: ')
+
+if k_sym_pass == '':
+    k_sym_pass = 8
+if quantity_pass == '':
+    quantity_pass = 1
+if symbol_password.lower() == 'clns':
+    n_cap = round(int(k_sym_pass) / 4)
+    n_low = round(int(k_sym_pass) / 4)
+    n_num = round(int(k_sym_pass) * 7 / 20)
+    n_sym = round(int(k_sym_pass) * 3 / 20)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                    fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cln':
+
+    n_cap = round(int(k_sym_pass) / 4)
+    n_low = round(int(k_sym_pass) / 4)
+    n_num = round(int(k_sym_pass) / 2)
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cls':
+
+    n_cap = round(int(k_sym_pass) / 4)
+    n_low = round(int(k_sym_pass) / 4)
+    n_num = 0
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cns':
+
+    n_cap = round(int(k_sym_pass) / 4)
+    n_low = 0
+    n_num = round(int(k_sym_pass) / 4)
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'lns':
+
+    n_cap = 0
+    n_low = round(int(k_sym_pass) / 4)
+    n_num = round(int(k_sym_pass) / 4)
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cl' or symbol_password.lower() == 'lc':
+
+    n_cap = round(int(k_sym_pass) / 2)
+    n_low = round(int(k_sym_pass) / 2)
+    n_num = 0
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cn' or symbol_password.lower() == 'nc':
+
+    n_cap = round(int(k_sym_pass) / 2)
+    n_low = 0
+    n_num = round(int(k_sym_pass) / 2)
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'cs' or symbol_password.lower() == 'sc':
+
+    n_cap = round(int(k_sym_pass) / 2)
+    n_low = 0
+    n_num = 0
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'ls' or symbol_password.lower() == 'sl':
+
+    n_cap = 0
+    n_low = round(int(k_sym_pass) / 2)
+    n_num = 0
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'ln' or symbol_password.lower() == 'nl':
+
+    n_cap = 0
+    n_low = round(int(k_sym_pass) / 2)
+    n_num = round(int(k_sym_pass) / 2)
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'ns' or symbol_password.lower() == 'sn':
+
+    n_cap = 0
+    n_low = 0
+    n_num = round(int(k_sym_pass) / 2)
+    n_sym = round(int(k_sym_pass) / 2)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'c':
+
+    n_cap = k_sym_pass
+    n_low = 0
+    n_num = 0
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'l':
+
+    n_cap = 0
+    n_low = k_sym_pass
+    n_num = 0
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 'n':
+
+    n_cap = 0
+    n_low = 0
+    n_num = k_sym_pass
+    n_sym = 0
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+elif symbol_password.lower() == 's':
+
+    n_cap = 0
+    n_low = 0
+    n_num = 0
+    n_sym = k_sym_pass
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+else:
+
+    n_cap = round(int(k_sym_pass) / 4)
+    n_low = round(int(k_sym_pass) / 4)
+    n_num = round(int(k_sym_pass) * 7 / 20)
+    n_sym = round(int(k_sym_pass) * 3 / 20)
+    n = 0
+    print('Ваши пароли:', end=' ')
+    while n < int(quantity_pass):
+        password = get_password(n_cap, n_low, n_num, n_sym)
+        print(password, end='   ')
+        n += 1
+
+        if save_file == 'y':
+            with open('password.xml', 'a+', encoding='utf-8') as fw:
+                fw.writelines(f'{password}\n')  # Записывает в файл сгенерированный пароль.
+
+
 # print(password_str)
 #print(password_str1)
