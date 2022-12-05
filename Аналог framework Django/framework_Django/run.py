a155288c00a6484d6a9167214519cdd895b6494d
Belyakov Roman <belyakov1981@mail.ru>
2022-12-05 13:14:29 +0300

    update_step_5

--- Аналог framework Django/framework_Django/run.py
+++ Аналог framework Django/framework_Django/run.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 from roman_framework.main import Framework
-from urls import routes
+from views import routes
 
 # Создаем объект WSGI-приложения
 application = Framework(routes)
@@ -8,3 +8,8 @@ application = Framework(routes)
 with make_server('', 8080, application) as httpd:
     print("Запуск на порту 8080...")
     httpd.serve_forever()
+
+# Пробуем записать логи в файл.
+# with open('log.txt', 'a+', encoding='utf-8') as fa:
+#     for _ in a:
+#         fa.writelines(f'{_}\n')  # Записывает в файл ответ сервера.
--- Аналог framework Django/framework_Django/views.py
+++ Аналог framework Django/framework_Django/views.py
@@ -3,22 +3,26 @@ from datetime import date
 
 from roman_framework.templator import render
 from components.models import Engine
+from components.decorators import AppRoute
 
 site = Engine()
-
+routes = {}
 
 # Класс-контроллер - Главная страница
+@AppRoute(routes=routes, url='/')
 class Index:
     def __call__(self, request):
         return '200 OK', render('index.html', objects_list=site.categories)
 
 
 # Класс-контроллер - Страница "О проекте"
+@AppRoute(routes=routes, url='/about/')
 class About:
     def __call__(self, request):
         return '200 OK', render('about.html')
 
 # Класс-контроллер - Страница "Описание проекта"
+@AppRoute(routes=routes, url='/read_me/')
 class Description:
     def __call__(self, request):
         return '200 OK', 'In this project, I, Roman Belyakov, teach several courses on the basics of programming on ' \
@@ -27,24 +31,28 @@ class Description:
 
 
 # Класс-контроллер - Страница "Моя личная страница"
+@AppRoute(routes=routes, url= '/site/')
 class My_site:
     def __call__(self, request):
         return '200 OK', render('my_site.html')
 
 
 # Класс-контроллер - Страница "Генератор паролей"
+@AppRoute(routes=routes, url= '/genpass/')
 class Genpassword:
     def __call__(self, request):
         return '200 OK', render('genpassword.html')
 
 
 # Класс-контроллер - Страница "Страница проекта из шага 2"
+@AppRoute(routes=routes, url= '/step_2/')
 class Step_2:
     def __call__(self, request):
         return '200 OK', render('step_2.html')
 
 
 # Класс-контроллер - Страница "Расписания"
+@AppRoute(routes=routes, url='/study_programs/')
 class StudyPrograms:
     def __call__(self, request):
         return '200 OK', render('study-programs.html', data=date.today())
@@ -57,6 +65,7 @@ class NotFound404:
 
 
 # Класс-контроллер - Страница "Список курсов"
+@AppRoute(routes=routes, url='/courses-list/')
 class CoursesList:
     def __call__(self, request):
 
@@ -72,6 +81,7 @@ class CoursesList:
 
 
 # Класс-контроллер - Страница "Создать курс"
+@AppRoute(routes=routes, url='/create-course/')
 class CreateCourse:
     category_id = -1
 
@@ -109,6 +119,7 @@ class CreateCourse:
 
 
 # Класс-контроллер - Страница "Создать категорию"
+@AppRoute(routes=routes, url='/create-category/')
 class CreateCategory:
     def __call__(self, request):
 
@@ -140,6 +151,7 @@ class CreateCategory:
 
 
 # Класс-контроллер - Страница "Список категорий"
+@AppRoute(routes=routes, url='/category-list/')
 class CategoryList:
     def __call__(self, request):
         return '200 OK', render('category_list.html',
